2e68c9e38d15c1623d21dfada3a8998bfbd2b894
Cecilie Haugstvedt <haugstvedt@gmail.com>
2021-11-24 20:14:34 +0100

    Adding todo functionality

--- a11y-app/src/App.js
+++ a11y-app/src/App.js
@@ -1,11 +1,87 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
+function Todo({ todo, index, markTodo, removeTodo }) {
+  return (
+    <div
+      className="todo"
+      
+    >
+      <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>{todo.text}</span>
+      <div>
+        <button className="outline-success" onClick={() => markTodo(index)}>✓</button>{' '}
+        <button className="outline-danger" onClick={() => removeTodo(index)}>✕</button>
+      </div>
+    </div>
+  );
+}
+
+function FormTodo({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Add Todo:
+        <input type="text"  className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Add new todo" />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
+
 function App() {
+  const [todos, setTodos] = React.useState([
+    {
+      text: "Be nice",
+      isDone: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const markTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isDone = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <h3>TODO List </h3>
+        <FormTodo addTodo={addTodo} />
         <img src={logo} className="App-logo" />
+        <div>
+          {todos.map((todo, index) => (
+            <ul>
+                <Todo
+                key={index}
+                index={index}
+                todo={todo}
+                markTodo={markTodo}
+                removeTodo={removeTodo}
+                />
+            </ul>
+          ))}
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
