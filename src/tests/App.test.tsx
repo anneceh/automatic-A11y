9981e0461762809c7c5d434cfe2b8900d1ffb828
Cecilie Haugstvedt <haugstvedt@gmail.com>
2025-09-22 22:03:37 +0200

    moves setup to separate setup file

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,14 +1,8 @@
-import 'vitest-axe/extend-expect'
 import { render } from '@testing-library/react'
 import { expect, test } from 'vitest'
 
-import * as matchers from 'vitest-axe/matchers'
-import type { AxeMatchers } from 'vitest-axe/matchers'
-
 import axe from './axe-helper.tsx'
 import App from '../App.tsx'
-
-expect.extend(matchers)
 
 test('should not have any accessibility errors', async () => {
   const { container } = render(<App/>)
@@ -16,7 +10,3 @@ test('should not have any accessibility errors', async () => {
 	expect(results).toHaveNoViolations()
 })
 
-declare module "vitest" {
-  export interface Assertion extends AxeMatchers {}
-  export interface AsymmetricMatchersContaining extends AxeMatchers {}
-}
--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,7 +1,16 @@
-import { afterEach } from 'vitest'
+import { afterEach, expect } from 'vitest'
 import { cleanup } from '@testing-library/react'
-import '@testing-library/jest-dom'
+
+import * as matchers from 'vitest-axe/matchers'
+import type { AxeMatchers } from 'vitest-axe/matchers'
 
 afterEach(() => {
   cleanup()
 })
+
+expect.extend(matchers)
+
+declare module "vitest" {
+  export interface Assertion extends AxeMatchers {}
+  export interface AsymmetricMatchersContaining extends AxeMatchers {}
+}
